f4fd1bfe04d1b13814468ac4822f8ca55c9323ad
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2024-06-06 14:29:03 +0600

    make fname table collum editable

--- src/main/java/com/oop/mahadi/demotablev/TableVControler.java
+++ src/main/java/com/oop/mahadi/demotablev/TableVControler.java
@@ -6,7 +6,9 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -29,7 +31,13 @@ public class TableVControler {
         fNamefxid.setCellValueFactory(new PropertyValueFactory<Student,String>("fName"));
         lNamefxid.setCellValueFactory(new PropertyValueFactory<Student,String>("lName"));
         dobFxid.setCellValueFactory(new PropertyValueFactory<Student,LocalDate>("BirthYear"));
-tavbeViewfxid.setItems(observable());
+          tavbeViewfxid.setItems(observable());
+
+          tavbeViewfxid.setEditable(true);
+          fNamefxid.setCellFactory(TextFieldTableCell.forTableColumn());
+
+
+
 
     }
     public ObservableList<Student> observable(){
@@ -37,6 +45,14 @@ tavbeViewfxid.setItems(observable());
         Student s1=new Student("x","y",LocalDate.of(2000, Month.APRIL,23));
          obList.add(s1);
         return obList;
+
+    }
+
+
+    @FXML
+    public void changeFirstNameCellEvent(TableColumn.CellEditEvent editCell){
+        Student demoStu    =tavbeViewfxid.getSelectionModel().getSelectedItem();
+        demoStu.setfName(editCell.getNewValue().toString());
 
     }
 }
